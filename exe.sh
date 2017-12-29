3031e13b4c305f652e0d5418f3269ef727c8af94
hp <namstudionsk@gmail.com>
2017-12-30 01:46:31 +0700

    Fix

--- exe.sh
+++ exe.sh
@@ -3,3 +3,15 @@
 #	My true shell 
 #
 echo "42"
+echo "Zero $0"
+echo "First $1"
+echo "Second $2"
+echo "Third $3"
+echo "Fourth $4"
+echo "Fifth $5"
+echo "Sixth $6"
+echo "=================="
+echo "Count parametrov bliad nahui is $#"
+echo "All parametrzz $*"
+echo "=================="
+echo "All parametrzzz $@"
